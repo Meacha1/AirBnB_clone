e5dee0bb5f5d6da8883b295e524bb548f5bbccc9
Meacha1 <meachattd@gmail.com>
2023-02-12 21:28:58 +0000

    count

--- console.py
+++ console.py
@@ -151,5 +151,19 @@ class HBNBCommand(cmd.Cmd):
         setattr(instanceU, tokensU[2], tokensU[3])
         models.storage.save()
 
+    def do_count(self, line):
+        """Counts the instances of a class.
+        """
+        words = line.split(' ')
+        if not words[0]:
+            print("** class name missing **")
+        elif words[0] not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            matches = [
+                k for k in storage.all() if k.startswith(
+                    words[0] + '.')]
+            print(len(matches))
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
